5593aaff76dc1cdefa64c15834573b1c2000ef78
dagimaklilubussiness <dagimaklilubussiness@gmail.com>
2026-07-31 16:05:18 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/* ==============================================================
+   SSSDP — Firebase configuration (Fixed)
+============================================================== */
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB_cpdhs5409pafS7CUbfDKRGdB9gu55GI",
+  authDomain: "sheno-secondary-school.firebaseapp.com",
+  projectId: "sheno-secondary-school",
+  storageBucket: "sheno-secondary-school.firebasestorage.app",
+  messagingSenderId: "585306537782",
+  appId: "1:585306537782:web:815edc3647a826deb67d97",
+  measurementId: "G-TVPTZT2WTJ"
+};
